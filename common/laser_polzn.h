19ebf87f0df7004f7c21e92fd1be9e3094868177
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2012-07-17 19:58:05 -0400

    Updates to laser_polzn.c and laser_polzn.h

--- common/laser_polzn.h
+++ common/laser_polzn.h
@@ -4,9 +4,9 @@
 #include <gsl/gsl_complex.h>
 
 typedef struct laser_polzn_vec_t {
-  gsl_complex (*get) (struct laser_polzn_vect_t *e, const int p);
+  gsl_complex (*get) (const struct laser_polzn_vec_t *e, const int p);
   void (*set) (struct laser_polzn_vec_t *e, const int p, gsl_complex val);
-  void (*rotate) (struct laser_polzn_vec_t *e, const int p, 
+  void (*rotate) (struct laser_polzn_vec_t *e,
 		  const double phi, const double theta, const double chi);
   gsl_complex e[3];
 } laser_polzn_vec;
@@ -14,28 +14,30 @@ typedef struct laser_polzn_vec_t {
 void laser_polzn_vec_init_from_cart (laser_polzn_vec *e, const gsl_complex ex,
 				     const gsl_complex ey, const gsl_complex ez);
 
-
 gsl_complex laser_polzn_vec_get (const laser_polzn_vec *e, const int p);
+
+void laser_polzn_vec_set (laser_polzn_vec *e, const int p, const gsl_complex val);
 
 void laser_polzn_vec_free (laser_polzn_vec *e);
 
-laser_polzn_vec laser_polzn_vec_rotate (const laser_polzn_vec e,
-					const double phi,
-					const double theta,
-					const double chi);
+void laser_polzn_vec_rotate (laser_polzn_vec *e, const double phi,
+			     const double theta, const double chi);
 
-typedef struct laser_polzn_tensor
+typedef struct laser_polzn_tensor_t
 {
-  gsl_complex (*get) (struct laser_polzn_vec_t *e, const int p);
-  void (*set) (struct laser_polzn_vec_t *e, const int p, gsl_complex val);
+  gsl_complex (*get) (const struct laser_polzn_tensor_t *E, const int k, const int p);
+  void (*set) (struct laser_polzn_tensor_t *E, const int k, const int p, gsl_complex val);
   gsl_complex E[9];
 } laser_polzn_tensor;
 
-laser_polzn_tensor laser_polzn_tensor_calc (const laser_polzn_vec e1,
-					    const laser_polzn_vec e2);
-
-gsl_complex laser_polzn_tensor_get (const laser_polzn_tensor E,
+gsl_complex laser_polzn_tensor_get (const laser_polzn_tensor *E,
 				    const int k, const int p);
+
+void laser_polzn_tensor_set (laser_polzn_tensor *E, const int k, const int p,
+			     const gsl_complex val);
+
+void laser_polzn_tensor_init_from_vecs (laser_polzn_tensor *E, const laser_polzn_vec *e1,
+					const laser_polzn_vec *e2);
 
 void laser_polzn_tensor_free (laser_polzn_tensor * E);
 
