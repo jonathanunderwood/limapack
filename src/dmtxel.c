b835c4aed69cb02918ea6b2d004e0fa5845aef06
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2013-08-21 11:11:23 +0100

    Update dmtxel.[ch] to allow for half integer AM.

--- src/dmtxel.c
+++ src/dmtxel.c
@@ -1,4 +1,4 @@
-/* TODO: allow for 1/2 integer a.m. Use analytical formulas rather than 3js.*/
+/* TODO: Use analytical formulas rather than 3js.*/
 
 #include <stdlib.h>
 #include <math.h>
@@ -6,80 +6,87 @@
 #include <gsl/gsl_sf_coupling.h>
 #include "dmtxel.h"
 
-
-/* Useful warning messages. */
-/* This function performs the normal angular momentum checks that would be
-   performed in a sensible 3j calculation function as a safeguard. Because we're
-   treating only integer angular momentum here, no test for 2(J+k+Jp) being even
-   is required. */
+/* If DMTXEL_DEBUG is defined, this function performs the normal
+   angular momentum checks that would be performed in a sensible 3j
+   calculation function as a safeguard. Because we're treating only
+   integer angular momentum here, no test for 2(J+k+Jp) being even is
+   required. */
 /* #define DMTXEL_DEBUG 1 */
 
 #ifdef DMTXEL_DEBUG
 #include <stdio.h>
-#define NAN 0.0/0.0 /* Not entirely portable. */
+#define __NAN 0.0/0.0		/* Not entirely portable. */
+#define __IS_ODD(n) ((n) & 1)
 #endif /* DMTXEL_DEBUG */
 
 double
-dmtxel (const int J, const int K, const int M, const int Jp, const int Kp,
-	const int Mp, const int k, const int p, const int q)
-     /* Matrix element of the rotation matrix <J K M|D^k_pq|Jp Kp Mp>.  
-	Currently only defined for integer angular momentum. */
+dmtxel (const int two_J, const int two_K, const int two_M,
+	const int two_Jp, const int two_Kp, const int two_Mp,
+	const int two_k, const int two_p, const int two_q)
+/* Matrix element of the rotation matrix <JKM|D^k_pq|JpKpMp>.
+   Arguments passed are twice the angular momenta.*/
 {
 
 #ifdef DMTXEL_DEBUG
-  if (p != Mp - M)
+  if (two_p != two_Mp - two_M)
     {
       fprintf (stderr, "dmtxel warning: p != Mp - M.\n");
       return 0.0;
     }
-  else if (q != Kp - K)
+  else if (two_q != two_Kp - two_K)
     {
       fprintf (stderr, "dmtxel warning: q != Kp - K.\n");
       return 0.0;
     }
-  else if (abs (q) > k)
+  else if (abs (two_q) > two_k)
     {
       fprintf (stderr, "dmtxel warning: abs(q) > k.\n");
-      return NAN;
+      return __NAN;
     }
-  else if (abs (p) > k)
+  else if (abs (two_p) > two_k)
     {
       fprintf (stderr, "dmtxel warning: abs(p) > k.\n");
-      return NAN;
+      return __NAN;
     }
-  else if (abs (M) > J)
+  else if (abs (two_M) > two_J)
     {
       fprintf (stderr, "dmtxel warning: abs(M) > J.\n");
-      return NAN;
+      return __NAN;
     }
-  else if (abs (K) > J)
+  else if (abs (two_K) > two_J)
     {
       fprintf (stderr, "dmtxel warning: abs(K) > J.\n");
-      return NAN;
+      return __NAN;
     }
-  else if (abs (Mp) > Jp)
+  else if (abs (two_Mp) > two_Jp)
     {
       fprintf (stderr, "dmtxel warning: abs(Mp) > Jp.\n");
-      return NAN;
+      return __NAN;
     }
-  else if (abs (Kp) > Jp)
+  else if (abs (two_Kp) > two_Jp)
     {
       fprintf (stderr, "dmtxel warning: abs(Kp) > Jp.\n");
-      return NAN;
+      return __NAN;
     }
-  else if ((Jp > J + k) || (Jp < abs (J - k)))
+  else if ((two_Jp > two_J + two_k) || (two_Jp < abs (two_J - two_k)))
     {
       fprintf (stderr, "dmtxel warning: (J, k, Jp) not triangle.\n");
       return 0.0;
     }
-#undef NAN
+  else if (__IS_ODD (two_J + two_k + two_Jp))
+    {
+      fprintf (stderr, "dmtxel warning: 2(J+k+Jp) is odd.\n");
+      return 0.0;
+    }
+#undef __IS_ODD
+#undef __NAN
 #endif /* DMTXEL_DEBUG */
 
-  double a = sqrt ((2.0 * J + 1.0) * (2.0 * Jp + 1.0)) *
-    gsl_sf_coupling_3j (2 * J, 2 * k, 2 * Jp, 2 * M, 2 * p, -2 * Mp) *
-    gsl_sf_coupling_3j (2 * J, 2 * k, 2 * Jp, 2 * K, 2 * q, -2 * Kp);
+  double a = sqrt ((two_J + 1.0) * (two_Jp + 1.0)) *
+    gsl_sf_coupling_3j (two_J, two_k, two_Jp, two_M, two_p, -two_Mp) *
+    gsl_sf_coupling_3j (two_J, two_k, two_Jp, two_K, two_q, -two_Kp);
 
-  if (GSL_IS_ODD (Mp - Kp))	/* phase */
+  if (GSL_IS_ODD (two_Mp - two_Kp))	/* phase */
     return -a;
   else
     return a;
--- src/dmtxel.h
+++ src/dmtxel.h
@@ -1,9 +1,10 @@
-/* Matrix elements of the Wigner rotation matrix elements. */
 #ifndef __DMTXEL_H__
 #define __DMTXEL_H__
 
-double dmtxel (const int J, const int K, const int M,
-	       const int Jp, const int Kp, const int Mp,
-	       const int k, const int p, const int q);
+/* Matrix elements of the Wigner rotation matrix elements. Arguments
+   passed are twice the angular momenta. */
+double dmtxel (const int two_J, const int two_K, const int two_M,
+	       const int two_Jp, const int two_Kp, const int two_Mp,
+	       const int two_k, const int two_p, const int two_q);
 
 #endif /* __DMTXEL_H__ */
