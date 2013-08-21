233982e7f2c527604f49ef3aeaeea38040c0c0f1
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2013-08-21 11:28:15 +0100

    Update dcmsq.[ch] to allow half integer AM.

--- src/dcmsq.c
+++ src/dcmsq.c
@@ -24,7 +24,7 @@ struct _dcmsq_expval
 dcmsq_expval_t *
 dcmsq_expval_ctor ()
 {
-  dcmsq_expval_t * dcmsq = NULL;
+  dcmsq_expval_t *dcmsq = NULL;
 
   if (MEMORY_ALLOC (dcmsq) < 0)
     {
@@ -41,10 +41,10 @@ dcmsq_expval_dtor (dcmsq_expval_t * dcmsq)
   MEMORY_FREE (dcmsq);
 }
 
-dcmsq_mtxel_t * 
+dcmsq_mtxel_t *
 dcmsq_mtxel_ctor ()
 {
-  dcmsq_mtxel_t * dcmsq = NULL;
+  dcmsq_mtxel_t *dcmsq = NULL;
 
   if (MEMORY_ALLOC (dcmsq) < 0)
     {
@@ -55,50 +55,44 @@ dcmsq_mtxel_ctor ()
   return dcmsq;
 }
 
-void 
+void
 dcmsq_mtxel_dtor (dcmsq_mtxel_t * dcmsq)
 {
   MEMORY_FREE (dcmsq);
 }
 
 void
-dcmsq_mtxel_calc (dcmsq_mtxel_t * mtxel, 
-		  const int J, const int K, const int M,
-		  const int Jp, const int Kp, const int Mp)
+dcmsq_mtxel_calc (dcmsq_mtxel_t * mtxel, const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
 /* Calculates the matrix elements of the squared direction cosine
    matrix elements <J K M| <A|b>^2 |Jp Kp Mp> for A = X, Y, Z and b =
-   x, y, z. Integer angular momentum only at this point. */
+   x, y, z. */
 {
   dcmsq_axes_t idx;
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double f = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 2);
-  double g = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, -2);
-  double h = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 2);
-  double i = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double f = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 4);
+  double g = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, -4);
+  double h = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 4);
+  double i = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   /* <J K Mx| <X|x>^2 |Jp Kp Mp> */
-  mtxel->data[idx = Xx] = (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 -
-    (b + c + d + e) / (2.0 * sqrt (6.0));
+  mtxel->data[idx = Xx] = (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 - (b + c + d + e) / (2.0 * sqrt (6.0));
 
   /* <J K M| <X|y>^2 |Jp Kp Mp> */
-  mtxel->data[idx = Xy] = (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 -
-    (d + e - b - c) / (2.0 * sqrt (6.0));
+  mtxel->data[idx = Xy] = (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 - (d + e - b - c) / (2.0 * sqrt (6.0));
 
   /* <J K M| <X|z>^2 |Jp Kp Mp> */
   mtxel->data[idx = Xz] = (j - a) / 3.0 + (d + e) / sqrt (6.0);
 
   /* <J K M| <Y|x>^2 |Jp Kp Mp> */
-  mtxel->data[idx = Yx] = (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 -
-    (b + c - d - e) / (2.0 * sqrt (6.0));
+  mtxel->data[idx = Yx] = (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 - (b + c - d - e) / (2.0 * sqrt (6.0));
 
   /* <J K M| <Y|y>^2 |Jp Kp Mp> */
-  mtxel->data[idx = Yy] = (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 +
-    (b + c - d - e) / (2.0 * sqrt (6.0));
+  mtxel->data[idx = Yy] = (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 + (b + c - d - e) / (2.0 * sqrt (6.0));
 
   /* <J K M| <Y|z>^2 |Jp Kp Mp> */
   mtxel->data[idx = Yz] = (j - a) / 3.0 - (d + e) / sqrt (6.0);
@@ -115,9 +109,8 @@ dcmsq_mtxel_calc (dcmsq_mtxel_t * mtxel,
   return;
 }
 
-void dcmsq_expval_add_mtxel_weighted_complex (dcmsq_expval_t *expval, 
-					      const dcmsq_mtxel_t *mtxel,
-					      const gsl_complex weight)
+void
+dcmsq_expval_add_mtxel_weighted_complex (dcmsq_expval_t * expval, const dcmsq_mtxel_t * mtxel, const gsl_complex weight)
 /* For each expectation value add to it the matrix element in mtxel
    multiplied by the complex coefficient weight. 
 
@@ -127,18 +120,15 @@ void dcmsq_expval_add_mtxel_weighted_complex (dcmsq_expval_t *expval,
 
   for (i = 0; i < 9; i++)
     {
-      gsl_complex weighted_mtxel = 
-	gsl_complex_mul_real (weight, mtxel->data[i]);
+      gsl_complex weighted_mtxel = gsl_complex_mul_real (weight, mtxel->data[i]);
 
-      expval->data[i] = 
-	gsl_complex_add (expval->data[i], weighted_mtxel);
+      expval->data[i] = gsl_complex_add (expval->data[i], weighted_mtxel);
     }
   return;
 }
 
-void dcmsq_expval_add_weighted (dcmsq_expval_t *a, 
-				const dcmsq_expval_t *b,
-				const double weight)
+void
+dcmsq_expval_add_weighted (dcmsq_expval_t * a, const dcmsq_expval_t * b, const double weight)
 /* Add expectation values as a = a + weight * b */
 {
   int i;
@@ -152,8 +142,8 @@ void dcmsq_expval_add_weighted (dcmsq_expval_t *a,
   return;
 }
 
-void 
-dcmsq_expval_zero(dcmsq_expval_t *dcmsq)
+void
+dcmsq_expval_zero (dcmsq_expval_t * dcmsq)
 {
   int i;
   gsl_complex zero = gsl_complex_rect (0.0, 0.0);
@@ -169,61 +159,57 @@ dcmsq_expval_zero(dcmsq_expval_t *dcmsq)
    pointers, a different approach will be needed. */
 
 int
-dcmsq_expval_mpi_send(const dcmsq_expval_t *expval, 
-		      int dest, int tag, MPI_Comm comm)
+dcmsq_expval_mpi_send (const dcmsq_expval_t * expval, int dest, int tag, MPI_Comm comm)
 {
   /* Note that the MPI_Send function (and the whole of the MPI 2 API)
      doesn't use the const qualifier, so we have to recast to void
      here to drop the const. This is idiotic, but there ya go. This
      is fixed in the MPI 3 spec. */
-  return MPI_Send((void *) expval, sizeof(dcmsq_expval_t), MPI_BYTE, dest, tag, comm);
+  return MPI_Send ((void *) expval, sizeof (dcmsq_expval_t), MPI_BYTE, dest, tag, comm);
 }
 
 int
-dcmsq_expval_mpi_recv(dcmsq_expval_t *expval, 
-		      int dest, int tag, MPI_Comm comm)
+dcmsq_expval_mpi_recv (dcmsq_expval_t * expval, int dest, int tag, MPI_Comm comm)
 {
-  return MPI_Recv(expval, sizeof(dcmsq_expval_t), MPI_BYTE, dest, 
-		  tag, comm, MPI_STATUS_IGNORE);
+  return MPI_Recv (expval, sizeof (dcmsq_expval_t), MPI_BYTE, dest, tag, comm, MPI_STATUS_IGNORE);
 }
 #endif
 
 int
-dcmsq_fwrite(const dcmsq_expval_t *expval, const hid_t *location)
+dcmsq_fwrite (const dcmsq_expval_t * expval, const hid_t * location)
 {
   hid_t cmplx_type;
   int status;
   /* Data space variables. */
   hid_t dataspace_id;
   int rank = 1;
-  hsize_t dim[1] = {9}; 
+  hsize_t dim[1] = { 9 };
   /* Data set variables. */
   hid_t dataset_id;
 
   /* Define and hdf5 compound data type to represent a gsl_complex type. */
-  cmplx_type = hdf5_gsl_complex_type_create();
+  cmplx_type = hdf5_gsl_complex_type_create ();
   if (cmplx_type < 0)
     {
-      fprintf(stderr, "%s %d: Failed to create HDF5 complex type.\n", __func__, __LINE__);
+      fprintf (stderr, "%s %d: Failed to create HDF5 complex type.\n", __func__, __LINE__);
       return -1;
     }
 
   /* Create dataspace. */
-  dataspace_id = H5Screate_simple(rank, dim, NULL);
+  dataspace_id = H5Screate_simple (rank, dim, NULL);
   if (dataspace_id < 0)
     {
-      fprintf(stderr, "%s %d: Failed to create dataspace.\n", __func__, __LINE__);
+      fprintf (stderr, "%s %d: Failed to create dataspace.\n", __func__, __LINE__);
       return -1;
     }
 
   /* Create dataset. Note that we don't have a leading "/" as this
      would force the data set into the root of the file, whereas
      location may refer to a group. */
-  dataset_id = H5Dcreate(*location, "dcmsq", cmplx_type, dataspace_id, 
-			 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  dataset_id = H5Dcreate (*location, "dcmsq", cmplx_type, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
   if (dataset_id < 0)
     {
-      fprintf(stderr, "%s %d: Failed to create dataset.\n", __func__, __LINE__);
+      fprintf (stderr, "%s %d: Failed to create dataset.\n", __func__, __LINE__);
       return -1;
     }
 
@@ -231,7 +217,7 @@ dcmsq_fwrite(const dcmsq_expval_t *expval, const hid_t *location)
   status = H5Dwrite (dataset_id, cmplx_type, H5S_ALL, H5S_ALL, H5P_DEFAULT, expval);
   if (status)
     {
-      fprintf(stderr, "%s %d: Failed to write to dataset.\n", __func__, __LINE__);
+      fprintf (stderr, "%s %d: Failed to write to dataset.\n", __func__, __LINE__);
       return -1;
     }
   /* Cleanup. */
@@ -243,145 +229,132 @@ dcmsq_fwrite(const dcmsq_expval_t *expval, const hid_t *location)
 }
 
 double
-dcmsq_Xx (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Xx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <X|x>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double f = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 2);
-  double g = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, -2);
-  double h = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 2);
-  double i = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double f = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 4);
+  double g = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, -4);
+  double h = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 4);
+  double i = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
-  return (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 -
-    (b + c + d + e) / (2.0 * sqrt (6.0));
+  return (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 - (b + c + d + e) / (2.0 * sqrt (6.0));
 }
 
 double
-dcmsq_Xy (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Xy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <X|y>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double f = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 2);
-  double g = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, -2);
-  double h = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 2);
-  double i = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double f = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 4);
+  double g = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, -4);
+  double h = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 4);
+  double i = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
-  return (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 -
-    (d + e - b - c) / (2.0 * sqrt (6.0));
+  return (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 - (d + e - b - c) / (2.0 * sqrt (6.0));
 }
 
 double
-dcmsq_Xz (const int J, const int K, const int M, const int Jp, const int Kp,
-	  const int Mp)
+dcmsq_Xz (const int two_J, const int two_K, const int two_M, const int Jp, const int Kp, const int Mp)
      /* <J K M| <X|z>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   return (j - a) / 3.0 + (d + e) / sqrt (6.0);
 }
 
 double
-dcmsq_Yx (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Yx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <Y|x>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double f = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 2);
-  double g = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, -2);
-  double h = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 2);
-  double i = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double f = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 4);
+  double g = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, -4);
+  double h = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 4);
+  double i = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
-  return (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 -
-    (b + c - d - e) / (2.0 * sqrt (6.0));
+  return (j / 3.0) + a / 6.0 - (f + g + h + i) / 4.0 - (b + c - d - e) / (2.0 * sqrt (6.0));
 
 }
 
 double
-dcmsq_Yy (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Yy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <Y|y>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double f = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 2);
-  double g = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, -2);
-  double h = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 2);
-  double i = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double f = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 4);
+  double g = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, -4);
+  double h = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 4);
+  double i = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
-  return (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 +
-    (b + c - d - e) / (2.0 * sqrt (6.0));
+  return (j / 3.0) + a / 6.0 + (f + g + h + i) / 4.0 + (b + c - d - e) / (2.0 * sqrt (6.0));
 }
 
 double
-dcmsq_Yz (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Yz (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
 	  /* <J K M| <Y|z>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double d = dmtxel (J, K, M, Jp, Kp, Mp, 2, 2, 0);
-  double e = dmtxel (J, K, M, Jp, Kp, Mp, 2, -2, 0);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double d = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 4, 0);
+  double e = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, -4, 0);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   return (j - a) / 3.0 - (d + e) / sqrt (6.0);
 }
 
 double
-dcmsq_Zx (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Zx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <Z|x>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   return (j - a) / 3.0 + (c - b) / sqrt (6.0);
 }
 
 double
-dcmsq_Zy (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Zy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <Z|y>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double b = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 2);
-  double c = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, -2);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double b = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 4);
+  double c = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, -4);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   return (j - a) / 3.0 - (b + c) / sqrt (6.0);
 }
 
 double
-dcmsq_Zz (const int J, const int K, const int M,
-	  const int Jp, const int Kp, const int Mp)
+dcmsq_Zz (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp)
      /* <J K M| <Z|z>^2 |Jp Kp Mp> */
 {
-  double a = dmtxel (J, K, M, Jp, Kp, Mp, 2, 0, 0);
-  double j = dmtxel (J, K, M, Jp, Kp, Mp, 0, 0, 0);
+  double a = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 4, 0, 0);
+  double j = dmtxel (two_J, two_K, two_M, two_Jp, two_Kp, two_Mp, 0, 0, 0);
 
   return (j + 2.0 * a) / 3.0;
 }
--- src/dcmsq.h
+++ src/dcmsq.h
@@ -6,69 +6,52 @@
 #include <hdf5.h>
 
 typedef enum dcmsq_axes
-{ Xx, Xy, Xz, Yx, Yy, Yz, Zx, Zy, Zz } 
+{ Xx, Xy, Xz, Yx, Yy, Yz, Zx, Zy, Zz }
 dcmsq_axes_t;
 
 typedef struct _dcmsq_mtxel dcmsq_mtxel_t;
 typedef struct _dcmsq_expval dcmsq_expval_t;
 
-dcmsq_mtxel_t * dcmsq_mtxel_ctor ();
+dcmsq_mtxel_t *dcmsq_mtxel_ctor ();
 void dcmsq_mtxel_dtor (dcmsq_mtxel_t * dcmsq_mtxel);
 
-dcmsq_expval_t * dcmsq_expval_ctor ();
+dcmsq_expval_t *dcmsq_expval_ctor ();
 void dcmsq_expval_dtor (dcmsq_expval_t * dcmsq_expval);
 
-void dcmsq_mtxel_calc (dcmsq_mtxel_t *mtxel,
-		       const int J, const int K, const int M,
-		       const int Jp, const int Kp, const int Mp);
+void dcmsq_mtxel_calc (dcmsq_mtxel_t * mtxel, const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-void dcmsq_expval_add_mtxel_weighted_complex (dcmsq_expval_t *expval, 
-					      const dcmsq_mtxel_t *mtxel,
-					      const gsl_complex weight);
+void dcmsq_expval_add_mtxel_weighted_complex (dcmsq_expval_t * expval, const dcmsq_mtxel_t * mtxel, const gsl_complex weight);
 
-void dcmsq_expval_add_weighted (dcmsq_expval_t *expval_a, 
-				const dcmsq_expval_t *expval_b,
-				const double weight);
+void dcmsq_expval_add_weighted (dcmsq_expval_t * expval_a, const dcmsq_expval_t * expval_b, const double weight);
 
-void dcmsq_expval_zero(dcmsq_expval_t *dcmsq);
+void dcmsq_expval_zero (dcmsq_expval_t * dcmsq);
 
-int dcmsq_fwrite(const dcmsq_expval_t *expval, const hid_t *location);
+int dcmsq_fwrite (const dcmsq_expval_t * expval, const hid_t * location);
 
 #ifdef BUILD_WITH_MPI
 #include <mpi.h>
-int dcmsq_expval_mpi_send(const dcmsq_expval_t *expval, 
-			  int dest, int tag, MPI_Comm comm);
+int dcmsq_expval_mpi_send (const dcmsq_expval_t * expval, int dest, int tag, MPI_Comm comm);
 
-int dcmsq_expval_mpi_recv(dcmsq_expval_t *expval, 
-			  int dest, int tag, MPI_Comm comm);
+int dcmsq_expval_mpi_recv (dcmsq_expval_t * expval, int dest, int tag, MPI_Comm comm);
 #endif
 
 //double dcmsq_get(const dcmsq_buff_t *buff, const dcmsq_axes_t axes);
 
-double dcmsq_Xx (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Xx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Xy (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Xy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Xz (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Xz (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Yx (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Yx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Yy (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Yy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Yz (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Yz (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Zx (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Zx (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Zy (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Zy (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 
-double dcmsq_Zz (const int J, const int K, const int M,
-		 const int Jp, const int Kp, const int Mp);
+double dcmsq_Zz (const int two_J, const int two_K, const int two_M, const int two_Jp, const int two_Kp, const int two_Mp);
 #endif /* __DCMSQ_H__ */
